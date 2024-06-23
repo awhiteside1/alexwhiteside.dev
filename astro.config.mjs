ea0a7b9f221b2c3efc49e0ef285a0d6b18981d02
Alex Whiteside <alex.whiteside1@gmail.com>
2024-06-23 11:46:07 -0400

    Update Astro adapter and dependencies for Vercel
    Switched Astro's adapter from Node to Vercel's serverless version. Updated the relevant dependencies in package.json and pnpm-lock.yaml. This change is

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
+import vercelServerless from '@astrojs/vercel/serverless';
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, envField } from "astro/config";
@@ -7,7 +7,7 @@ import { defineConfig, envField } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astro-sphere-demo.vercel.app",
-	adapter: node({ mode: "standalone" }),
+	adapter: vercelServerless(),
 	integrations: [mdx(), sitemap(), preact({ compat: true })],
 	output: "server",
 	vite: {
