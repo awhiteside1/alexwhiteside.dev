3ead47456d87c87218bee5997f8bd274b01d7a5e
Alex Whiteside <alex.whiteside1@gmail.com>
2024-07-15 14:12:24 -0400

    Updates

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
