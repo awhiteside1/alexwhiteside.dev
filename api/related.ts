327bf8a293797c15236b809f660f42a1ae863566
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-03 01:55:37 -0400

    Surreal

--- api/related.ts
+++ api/related.ts
@@ -6,7 +6,6 @@ const getSecrets = () => {
     const S3_SECRET = process.env.CLOUDFLARE_S3_SECRET
     if (!NOMIC_API_LEY) throw new Error('Missing NOMIC API')
     if (!S3_SECRET) throw new Error('Missing S3 Secret API')
-    console.log(NOMIC_API_LEY, S3_SECRET)
     return {NOMIC_API_LEY, S3_SECRET}
 }
 
@@ -15,14 +14,15 @@ export default async function (request: VercelRequest, response: VercelResponse)
     if (!term) return response.status(400).json({error: 'Missing term'})
     const termString = typeof term === 'string' ? term : term[0]
     try {
-        const connection = await lancedb.connect('s3://alexwhitesidedev', {
+        const connection = await lancedb.connect('s3://alexwhitesidedev/', {
             storageOptions: {
                 awsAccessKeyId: "e211da2bae41c27edf7b98498f1c4e2a",
                 endpoint: "https://4e48ace160a6411277cc07341caa999d.r2.cloudflarestorage.com",
                 awsSecretAccessKey: getSecrets().S3_SECRET!,
-                awsRegion: 'auto'
+                region: "auto",
             }
         })
+        console.log(await connection.tableNames())
 
         const table = await connection.openTable('cache')
         const results = await table.query().nearestTo(await fetchEmbedding(termString)).column('embedding').distanceType('cosine').limit(10).select(['repos', 'original']).bypassVectorIndex().toArray()
--- src/github/related.spec.ts
+++ src/github/related.spec.ts
@@ -1,9 +1,10 @@
-
+import fun from '../../api/related'
 import {describe,it} from "vitest";
 
 describe('fdssf', () => {
     it('should ', async() => {
-
+        //@ts-ignore
+        const results = await fun({query:{term:'css'} }, {json:console.log})
 
 
     });
