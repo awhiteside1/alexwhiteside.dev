6800846145173797f2afc4fccab409e27f58f62d
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-03 01:45:59 -0400

    Surreal

--- src/github/related.spec.ts
+++ src/github/related.spec.ts
@@ -1,10 +1,10 @@
-import {fetchEmbedding} from '../../api/related.ts'
+
 import {describe,it} from "vitest";
 
 describe('fdssf', () => {
     it('should ', async() => {
 
-        const x = await fetchEmbedding('term')
-        console.log(x)
+
+
     });
 });
