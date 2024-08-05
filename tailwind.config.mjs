18c2f3eb449b194d254a8365415700fd5fe5105d
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-05 17:27:25 -0400

    Sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -67,7 +67,7 @@ export default {
 				{
 					'bg-grid': (value) => ({
 						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="28" height="28" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
 						)}")`,
 					}),
 					'bg-grid-small': (value) => ({
