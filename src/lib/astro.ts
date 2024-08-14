80913df185b91e6b7dc39f7fd94b3ec08eed6440
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-14 13:17:42 -0400

    animation

---
+++ src/lib/astro.ts
@@ -0,0 +1,37 @@
+import defu from 'defu'
+import { uid } from 'radash'
+
+declare global {
+    interface Window {
+        astroInitContexts: Map<string, Map<string, any>>
+    }
+}
+
+interface Props {
+    pages?: Array<string>
+    id?: string
+}
+
+export const ensureInit = <T extends Map<string, any>>(
+    fn: (document: Document, context: T) => void,
+    props?: Props
+) => {
+    const propss = defu(props, { id: uid(7) })
+    const context = new Map()
+    window.astroInitContexts.set(propss.id, context)
+
+    document.addEventListener('astro:page-load', (ev) => {
+        const document = window.document
+        const pathname = window.location.pathname
+        if (propss.pages && !propss.pages.includes(pathname)) {
+            console.log('Skipping init for ', pathname)
+            return
+        }
+        const updatedContext = window.astroInitContexts.get(propss.id) as T
+        try {
+            fn(document, updatedContext)
+        } catch (err) {
+            console.error(err)
+        }
+    })
+}
