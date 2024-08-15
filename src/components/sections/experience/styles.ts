9da496b7908cb59a674bf339ac5457b334169d92
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-14 22:19:14 -0400

    sizes

--- src/components/sections/experience/styles.ts
+++ src/components/sections/experience/styles.ts
@@ -2,10 +2,9 @@ import { css } from '@styles/css'
 
 export const stacked = css({
     position: 'absolute',
-    inset: '10px',
+    inset: '2px',
     borderRadius: '10px',
     backdropBlur: '2xl',
     opacity: 0,
     transition: 'all 0.3s ease-in-out',
-    transform: 'translateX(-10px)',
 })
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
-import { defineCollection } from 'astro/content/runtime'
+import { defineCollection } from 'astro:content'
 import { z } from 'astro/zod'
 
 const awesome = defineCollection({
-	type: 'content',
-	schema: z.object({
-		related: z.array(z.string()).default([]),
-		topic: z.string().optional(),
-		name: z.string(),
-		featured: z.boolean().default(false),
-		author: z.string(),
-		url: z.string().url(),
-		cover: z.string().url().optional(),
-		description: z.string(),
-		date: z.coerce.date().optional(),
-		draft: z.boolean().default(false),
-	}),
+    type: 'content',
+    schema: z.object({
+        related: z.array(z.string()).default([]),
+        topic: z.string().optional(),
+        name: z.string(),
+        featured: z.boolean().default(false),
+        author: z.string(),
+        url: z.string().url(),
+        cover: z.string().url().optional(),
+        description: z.string(),
+        date: z.coerce.date().optional(),
+        draft: z.boolean().default(false),
+    }),
 })
 
 // const tech = defineCollection({
@@ -31,35 +31,36 @@ const awesome = defineCollection({
 // })
 
 const work = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		company: z.string(),
-		from: z.coerce.date(),
-		to: z.coerce.date().optional(),
-		stack: z.string().transform((value) => value.split(' ')),
-		logo: z.string().url(),
-	}),
+    type: 'content',
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            company: z.string(),
+            from: z.coerce.date(),
+            to: z.coerce.date().optional(),
+            stack: z.string().transform((value) => value.split(' ')),
+            logo: z.string().url().or(image()),
+        }),
 })
 
 const titles = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		color: z.string(),
-		short: z.string(),
-		order: z.number(),
-		className: z.string().optional(),
-	}),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        color: z.string(),
+        short: z.string(),
+        order: z.number(),
+        className: z.string().optional(),
+    }),
 })
 
 const sections = defineCollection({
-	type: 'content',
-	schema: z.object({
-		page: z.enum(['home', 'blog', 'background', 'books']).optional(),
-		name: z.string().optional(),
-		classes: z.string().optional(),
-	}),
+    type: 'content',
+    schema: z.object({
+        page: z.enum(['home', 'blog', 'background', 'books']).optional(),
+        name: z.string().optional(),
+        classes: z.string().optional(),
+    }),
 })
 
 export const collections = { awesome, work, titles, sections }
