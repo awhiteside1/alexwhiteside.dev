0b24770601c432cc396694a48ea682c186ded316
Alex Whiteside <alex.whiteside1@gmail.com>
2024-08-19 20:58:59 -0400

    📸✨ feat(experience): add image optimization for company logos in ExperienceSquare component

--- api/og/page.tsx
+++ api/og/page.tsx
@@ -1,25 +1,88 @@
+import type { VercelRequest } from '@vercel/node'
 import { ImageResponse } from '@vercel/og'
+import { match } from 'ts-pattern'
 
 export const config = {
 	runtime: 'edge',
 }
 
-export default async function handler() {
-	return new ImageResponse(
+type PageParameters = {
+	kind: 'page'
+	title: string
+	description: string
+}
+
+type PostParameters = {
+	kind: 'post'
+	title: string
+	coverUrl: string
+}
+
+type RequestParameters = PageParameters | PostParameters | { kind: undefined }
+
+const extractParametersFromRequest = (
+	request: VercelRequest,
+): RequestParameters => {
+	if (!request.url) return { kind: undefined }
+	const { searchParams } = new URL(request.url)
+	const data = Object.fromEntries(searchParams.entries())
+	return data as RequestParameters
+}
+
+export default async function handler(request: VercelRequest) {
+	const params = extractParametersFromRequest(request)
+
+	const display = match(params)
+		.with({ kind: 'page' }, (page) => {
+			return {
+				title: page.title,
+				image: undefined,
+			}
+		})
+		.with({ kind: 'post' }, (post) => {
+			return {
+				title: post.title,
+				image: post.coverUrl,
+			}
+		})
+		.otherwise(() => {
+			return {
+				title: 'Alex Whiteside',
+				image: undefined,
+			}
+		})
+
+	new ImageResponse(
 		<div
 			style={{
-				fontSize: 40,
-				color: 'black',
-				background: 'white',
-				width: '100%',
-				height: '100%',
-				padding: '50px 200px',
-				textAlign: 'center',
+				margin: 0,
+				padding: 0,
+				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
+				height: '100vh',
+				fontFamily: 'Arial, sans-serif',
+				background: 'linear-gradient(to bottom right, #f0f0f0, #ffffff)',
 			}}
 		>
-			Alex Whiteside
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					textAlign: 'center',
+				}}
+			>
+				<h1
+					style={{
+						fontSize: '3rem',
+						margin: '20px 20px',
+						flexGrow: 1,
+						textAlign: 'right',
+					}}
+				>
+					{display.title}
+				</h1>
+			</div>
 		</div>,
 		{
 			width: 1200,
