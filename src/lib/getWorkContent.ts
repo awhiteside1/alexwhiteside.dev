3bca0542ec39ce8c34f6eed332f2fd79b926b616
Alex Whiteside <alex.whiteside1@gmail.com>
2024-11-16 19:45:03 -0500

    Move Assets

--- src/lib/getWorkContent.ts
+++ src/lib/getWorkContent.ts
@@ -6,35 +6,39 @@ type Work = CollectionEntry<'work'>
 type WorkData = Work['data']
 
 type GroupedEntry = {
-    roles: Array<Work>
-    company: string
-    logo: string
-    from: Date
-    to: Date
+	roles: Array<Work>
+	company: string
+	logo: string
+	from: Date
+	to: Date
 }
 
 export const getWorkContent = async () => {
-    const allWork = await getCollection('work')
-    const grouped = group(allWork, (w) => w.data.company)
-    const byCompany = mapValues(
-        grouped,
-        (group) =>
-            group && {
-                roles: group?.toSorted(byDate((role) => role.data.from)),
-                company: first(sift(group.map((x) => x.data.company))),
-                logo: first(sift(group.map((x) => x.data.logo))),
-                from: first(group.map((x) => x.data.from).toSorted()),
-                to: last(group.map((x) => x.data.to).toSorted()),
-            }
-    )
+	const allWork = await getCollection('work')
+	return processWorkContent(allWork)
+}
 
-    const map = new Map(Object.entries(byCompany)) as Map<string, GroupedEntry>
-    return {
-        map,
-        getByCompany: (company: string) => {
-            const result = map.get(company)
-            if (result) return result
-            throw new Error('Not Found')
-        },
-    }
+export const processWorkContent = (allWork: Array<Work>) => {
+	const grouped = group(allWork, (w) => w.data.company)
+	const byCompany = mapValues(
+		grouped,
+		(group) =>
+			group && {
+				roles: group?.toSorted(byDate((role) => role.data.from)),
+				company: first(sift(group.map((x) => x.data.company))),
+				logo: first(sift(group.map((x) => x.data.logo))),
+				from: first(group.map((x) => x.data.from).toSorted()),
+				to: last(group.map((x) => x.data.to).toSorted()),
+			},
+	)
+
+	const map = new Map(Object.entries(byCompany)) as Map<string, GroupedEntry>
+	return {
+		map,
+		getByCompany: (company: string) => {
+			const result = map.get(company)
+			if (result) return result
+			throw new Error('Not Found')
+		},
+	}
 }
