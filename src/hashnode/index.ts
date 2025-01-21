4bda4648de432189eb86aea48912afca8e83f8be
Alex Whiteside <alex.whiteside1@gmail.com>
2025-01-21 16:12:03 -0500

    Add featured posts section and refine component behaviors
    Introduced a new "FeaturedPosts" section to display highlighted articles, utilizing data from the Hashnode API. Adjusted component imports and organized utility functions for consistency. Updated the icons file for better clarity and added metadata about icon sources.

--- src/hashnode/index.ts
+++ src/hashnode/index.ts
@@ -1,9 +1,9 @@
 import { makeClient } from './client'
-import { getPost, getPosts } from './queries'
-import { getPostByID } from './queries'
+import { getFeatured, getPost, getPostByID, getPosts } from './queries'
 
 export const Hashnode = {
 	getPosts: getPosts(makeClient),
 	getPostByID: getPostByID(makeClient),
 	getPost: getPost(makeClient),
+	getFeatured: getFeatured(makeClient),
 }
