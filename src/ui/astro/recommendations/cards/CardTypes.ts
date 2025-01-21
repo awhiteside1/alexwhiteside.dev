5cfa82b21805cb183475e2644b0ccbf35b0b9c9a
Alex Whiteside <alex.whiteside1@gmail.com>
2025-01-21 16:17:01 -0500

    Refactor CardTypes and talk.astro for consistency and readability
    - Standardized import formatting and spacing in CardTypes.ts and talk.astro.
    - Ensured consistent use of single and double quotes for strings.
    - Improved code readability by aligning object properties and function definitions.

--- src/ui/astro/recommendations/cards/CardTypes.ts
+++ src/ui/astro/recommendations/cards/CardTypes.ts
@@ -1,22 +1,22 @@
-import type { CollectionEntry } from 'astro:content'
+import type {CollectionEntry} from 'astro:content'
 
 export type CardTypeOptions = {
-	imageGenerationFn: (entry: CollectionEntry<'awesome'>) => string
-	showTitle: boolean
+    imageGenerationFn: (entry: CollectionEntry<'awesome'>) => string
+    showTitle: boolean
 }
 
 const defaultOptions: CardTypeOptions = {
-	imageGenerationFn: () => 'https://placehold.co/300x500',
-	showTitle: true,
+    imageGenerationFn: () => 'https://placehold.co/300x500',
+    showTitle: true,
 }
 
 export type CardKinds = 'blogs' | 'talks' | 'books'
 export const CardTypes: Record<CardKinds, CardTypeOptions> = {
-	blogs: defaultOptions,
-	books: { ...defaultOptions, showTitle: false },
-	talks: {
-		showTitle: true,
-		imageGenerationFn: (entry) =>
-			`https://i.ytimg.com/vi/${new URL(entry.data.url).pathname.replaceAll('/', '')}/hqdefault.jpg`,
-	},
+    blogs: defaultOptions,
+    books: {...defaultOptions, showTitle: false},
+    talks: {
+        showTitle: true,
+        imageGenerationFn: (entry) =>
+            `https://i.ytimg.com/vi/${new URL(entry.data.url).pathname.replaceAll('/', '')}/hqdefault.jpg`,
+    },
 }
